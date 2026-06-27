45256c638013c81e9b4793d74c1df715e9eeb6ff
vishalkummartank-blip <vishalkummartank@gmail.com>
2026-06-27 23:03:22 +0530

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,1 +1,94 @@
+// Firebase SDK
 
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+import { getAnalytics }
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+
+import {
+
+    getAuth,
+
+    GoogleAuthProvider,
+
+    GithubAuthProvider,
+
+    signInWithPopup,
+
+    signOut,
+
+    onAuthStateChanged
+
+}
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+
+// Providers
+
+const googleProvider =
+new GoogleAuthProvider();
+
+const githubProvider =
+new GithubAuthProvider();
+
+
+// Google Login
+
+window.loginGoogle = async () => {
+
+    try {
+
+        await signInWithPopup(auth, googleProvider);
+
+    } catch(err) {
+
+        console.error(err);
+
+    }
+
+};
+
+
+// GitHub Login
+
+window.loginGithub = async () => {
+
+    try {
+
+        await signInWithPopup(auth, githubProvider);
+
+    } catch(err) {
+
+        console.error(err);
+
+    }
+
+};
+
+
+// Logout
+
+window.logout = async () => {
+
+    await signOut(auth);
+
+};
+
+
+// User State
+
+onAuthStateChanged(auth, user => {
+
+    console.log(user);
+
+});
