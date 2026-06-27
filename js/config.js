31b592a294a0048354f8fe81656276d073285e9f
vishalkummartank-blip <vishalkummartank@gmail.com>
2026-06-27 14:32:35 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,21 @@
-const SUPABASE_URL = "https://nkppscojbtwtofdljjai.supabase.co";
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
 
-const SUPABASE_KEY =
-"sb_publishable_0GbdJP0v7YxOd5i5aHnw4Q_A-dCqRid";
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "XXX",
+    authDomain: "vishal-f0a01.firebaseapp.com",
+    projectId: "vishal-f0a01",
+    storageBucket: "vishal-f0a01.firebasestorage.app",
+    messagingSenderId: "XXX",
+    appId: "1:1073161731462:web:24dd8a1cadde0eea1f0007",
+    measurementId: "XXX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export so other JavaScript files can use Firebase
+export { app, analytics };
